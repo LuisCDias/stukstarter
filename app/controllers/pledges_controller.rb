a2c610724a5bc2f87eededc654d8e785ccfc6d73
Luís Dias <lcdias127@gmail.com>
2015-06-02 15:22:22 +0100

    Refactored Orders to Pledges using UserPledge

---
+++ app/controllers/pledges_controller.rb
@@ -0,0 +1,53 @@
+class PledgesController < ApplicationController
+	before_action :authenticate_user!
+  	before_action :set_project
+  	before_action :set_reward
+
+  	def index
+  		respond_to do |format|
+	      format.html
+	    end
+  	end
+
+  	def new
+  		@pledge = current_user.user_pledges.build
+  		@rewards = @project.rewards
+  		respond_to do |format|
+	      format.html
+	    end
+  	end
+
+  	def checkout
+  		@amount = pledge_params[:amount].to_i
+  		@pledge = current_user.user_pledges.build(reward:@reward, amount:@amount)
+  		@rewards = @project.rewards
+
+  		respond_to do |format|
+  			if @reward && @amount.present? && @reward.value <= @amount
+  				format.html
+  			else
+  				if @amount.present?
+  					flash.now[:error] = "The amount must have a value greater or equal to the reward"
+  				else
+  					flash.now[:error] = "You must provide an amount"
+  				end
+  				format.html { render :new}
+  			end			
+	    end
+  	end
+
+  	private
+  	
+  	def set_project
+		@project = Project.find(params[:project_id])
+	end
+
+	def set_reward		
+		id = params[:user_pledge] ? pledge_params[:reward] : params[:reward]
+	    @reward = @project.rewards.find_by_id(id)
+	end
+
+	def pledge_params
+		params.require(:user_pledge).permit(:reward, :amount)
+	end
+end
